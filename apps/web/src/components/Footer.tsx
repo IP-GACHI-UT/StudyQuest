f09b82d0a89f70f9977b0a34238f420b04043fa5
Radon0 <ryoto.enjoy@outlook.jp>
2026-06-07 18:44:54 +0900

    feature: フッタ作成

---
+++ apps/web/src/components/Footer.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+    return (
+        <footer className="bg-gray-800 text-white p-4 mt-8">
+            <p className="text-center">&copy; 2026 StudyQuest. All rights reserved.</p>
+        </footer>
+    );
+}
